c65149022033b113eccd16e66fa8163f1655b12b
kentakamata <kenta.kamata@icloud.com>
2024-01-15 14:56:12 +0900

    コードの制作

--- Assets/Kamata/Illumination.cs
+++ Assets/Kamata/Illumination.cs
@@ -6,13 +6,22 @@ using UnityEngine;
 
 public class Illumination : MonoBehaviour
 {
+    public GameObject remotecontroller;
+    public GameObject player;
     bool isActive;
 
     Light L;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
+        if (player == null)
+            player = GameObject.FindWithTag("Player");
+
+        //Instantiate(remotecontroller, player.transform.
+        //position, player.transform.rotation) as GameObject;
         L = GetComponent<Light>();
 
         isActive = L.isActiveAndEnabled;
